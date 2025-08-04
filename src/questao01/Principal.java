c6fd3e9294ce2898ae69e5ef751700046e648539
Jairton J. S. Barros <juninbarros94@hotmail.com>
2025-08-04 18:10:14 -0300

    Principal.java

--- src/questao01/Principal.java
+++ src/questao01/Principal.java
@@ -1,7 +1,17 @@
 package questao01;
+include java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int maior = (num1 > num2) ? num1:num2;
+
+        System.out.println("> O maior número é: " + maior);
+        scanner.close();
     }
 }
