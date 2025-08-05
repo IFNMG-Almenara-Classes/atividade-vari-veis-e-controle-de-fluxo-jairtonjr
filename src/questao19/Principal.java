6f7e931f6844b8bc74892a93a3badd1a0ecf34f4
Jairton J. S. Barros <juninbarros94@hotmail.com>
2025-08-05 17:21:38 -0300

    Principal.java

--- src/questao19/Principal.java
+++ src/questao19/Principal.java
@@ -1,3 +1,4 @@
+//Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a este número. Isto é, domingo se 1, segunda-feira se 2, e assim por diante.
 package questao19;
 import java.util.Scanner;
 
@@ -9,13 +10,20 @@ public class Principal {
 
         String nomeDia;
         switch (dia) {
-            case 1: nomeDia = "Domingo";break;
-            case 2: nomeDia = "Segunda-feira"; break;
-            case 3: nomeDia = "Terça-feira"; break;
-            case 4: nomeDia = "Quarta-feira"; break;
-            case 5: nomeDia = "Quinta-feira"; break;
-            case 6: nomeDia = "Sexta-feira"; break;
-            case 7: nomeDia = "Sábado"; break;
+            case 1: nomeDia = "Domingo";
+                break;
+            case 2: nomeDia = "Segunda-feira"; 
+                break;
+            case 3: nomeDia = "Terça-feira"; 
+                break;
+            case 4: nomeDia = "Quarta-feira";
+                break;
+            case 5: nomeDia = "Quinta-feira"; 
+                break;
+            case 6: nomeDia = "Sexta-feira"; 
+                break;
+            case 7: nomeDia = "Sábado"; 
+                break;
             default: nomeDia = "Número inválido";
         }
         System.out.println("> " + nomeDia);
