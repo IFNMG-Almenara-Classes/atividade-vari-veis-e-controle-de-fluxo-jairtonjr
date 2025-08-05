f70d76d4d1c1df380cbd9917586235bbfa455c52
Jairton J. S. Barros <juninbarros94@hotmail.com>
2025-08-05 16:11:21 -0300

    Principal.java

--- src/questao10/Principal.java
+++ src/questao10/Principal.java
@@ -1,8 +1,11 @@
+//Escrever um programa para exibir os números de 1 até 50 na tela usando o do..while.
 package questao10;
 
 public class Principal {
     public static void main(String[] args) {
+        
         int i = 1;
+        
         do {
             System.out.println(i);
             i++;
