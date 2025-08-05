5a5041adbeb13526c72776a6e7db6e86fd4d8dbe
Jairton J. S. Barros <juninbarros94@hotmail.com>
2025-08-05 16:20:29 -0300

    Principal.java

--- src/questao14/Principal.java
+++ src/questao14/Principal.java
@@ -1,3 +1,5 @@
+/*Uma pizzaria fornece 10% de desconto para funcionários e 5% de desconto para clientes VIP. Faça um programa que calcule o valor total a ser pago pela venda de uma pizza. 
+O programa deverá ler o valor da compra e um código que indica o tipo de cliente: 1-Comum, 2-VIP e 3-Funcionário.*/
 package questao14;
 import java.util.Scanner;
 
@@ -9,16 +11,16 @@ public class Principal {
         System.out.print("Digite o código do cliente (1-Comum, 2-VIP, 3-Funcionário): ");
         int tipo = scanner.nextInt();
 
-        double desconto = 0;
+        double desc = 0;
 
         switch (tipo) {
-            case 2: desconto = 0.05;
+            case 2: desc = 0.05;
             break;
-            case 3: desconto = 0.10;
+            case 3: desc = 0.10;
             break;
         }
 
-        double valorFinal = valor - (valor * desconto);
+        double valorFinal = valor - (valor * desc);
 
         System.out.printf("> Valor a pagar: R$ %.2f%n", valorFinal);
         scanner.close();
