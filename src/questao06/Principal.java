6b55c04e8532ee7fcd04568d069e0f5efacf6a12
Jairton J. S. Barros <juninbarros94@hotmail.com>
2025-08-05 11:51:54 -0300

    Add files via upload

---
+++ src/questao06/Principal.java
@@ -0,0 +1,26 @@
+package questao06;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Digite o terceiro número: ");
+        int num3 = scanner.nextInt();
+
+        int menor = num1;
+
+        if (num2 < menor) {
+            menor = num2;
+        }
+        if (num3 < menor) {
+            menor = num3;
+        }
+
+        System.out.println("> O menor número é " + menor);
+        scanner.close();
+    }
+}
